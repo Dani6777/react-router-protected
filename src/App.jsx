3c6bed1200f787f51d486065b7e1b4e54ceb67bd
Daniel Morales <daniel.morales23@outlook.cl>
2024-01-13 23:38:46 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Admin, Analytics, Dashboard, Home, Landing } from './pages';
+import { useState } from 'react';
+
+
+function App() {
+
+  const [user, setUser] = useState(null)
+
+  const login = () => {
+    //request done
+    setUser({
+      id: 1,
+      name: 'Daniel'
+    })
+  }
+  console.log(user)
+
+
+  const logout = () => setUser(null)
+  return (
+    <BrowserRouter>
+      <Navigation />
+      {
+        user ? (
+          <button onClick={logout}>Logout</button>
+        ) : (
+          <button onClick={login}>Login</button>
+        )
+      }
+
+
+      <Routes>
+        <Route index element={<Landing />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/analytics' element={<Analytics />} />
+        <Route path='/admin' element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Navigation() {
+  return <nav>
+    <ul>
+      <li>
+        <Link to='/landing'>Landing</Link>
+      </li>
+      <li>
+        <Link to='/home'>Home</Link>
+      </li>
+      <li>
+        <Link to='/dashboard'>Dashboard</Link>
+      </li>
+      <li>
+        <Link to='/analytics'>Analytics</Link>
+      </li>
+      <li>
+        <Link to='/admin'>Admin</Link>
+      </li>
+    </ul>
+  </nav>
+}
+
+export default App
---
+++ src/pages/index.jsx
@@ -0,0 +1,33 @@
+
+export const Landing = () => (
+    <div>
+        <h2>Landing Page (Public)</h2>
+        {/* Add content for the landing page */}
+    </div>
+);
+
+export const Home = () => {
+    return <h2>Home Page (Private)</h2>
+}
+
+
+export const Dashboard = () => (
+    <div>
+        <h2>Dashboard Page (Private)</h2>
+        {/* Add content for the dashboard page */}
+    </div>
+);
+
+export const Analytics = () => (
+    <div>
+        <h2>Analytics Page (Private, permission: analyze)</h2>
+        {/* Add content for the analytics page */}
+    </div>
+);
+
+export const Admin = () => (
+    <div>
+        <h2>Admin Page (Private, permission: admin)</h2>
+        {/* Add content for the admin page */}
+    </div>
+);
